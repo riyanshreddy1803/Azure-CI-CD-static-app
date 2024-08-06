b33a0e40320a5bb6638faac9e2998c9c48a31453
SunilReddy <sunilreddy7181@gmmail.com>
2024-08-07 01:05:07 +0530

    Changed the Title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>My Ai App</h1>
+    <h1>Gen AI for Riyansh & Savic Business</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
