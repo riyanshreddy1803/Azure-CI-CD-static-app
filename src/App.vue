4bea2b6cf7bd8d86c68ef2ab70bf9b1b0bd3375e
SunilReddy <sunilreddy7181@gmmail.com>
2024-08-07 12:35:11 +0530

    Added ML Ops to the title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Gen AI for Riyansh & Savic Business</h1>
+    <h1>Gen AI and ML Ops</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
